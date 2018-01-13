8113fb3637e7e3e75e481da22d9522b8878e0f50
lucas_j <lucas_j@LT2292.group.wan>
2018-01-13 16:10:20 +0100

    Post message on change in JET app to parent window

--- ViewController/public_html/jet-web-app/real-jet-app-sources/js/viewModels/workarea.js
+++ ViewController/public_html/jet-web-app/real-jet-app-sources/js/viewModels/workarea.js
@@ -1,7 +1,7 @@
 
 define(
-    ['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojinputtext'    
-],
+    ['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojinputtext'
+    ],
     function (oj, ko, $) {
         'use strict';
         function WorkareaViewModel() {
@@ -9,19 +9,50 @@ define(
             // initialize two country observables
             self.country = ko.observable("Italy");
 
-            self.init = function() {
+            self.callParent = function (message) {
+                console.log('send message from Web App to parent window');
+                // here we can restrict which parent page can receive our message
+                // by specifying the origin that this page should have
+                var targetOrigin = '*';
+                parent.postMessage(message, targetOrigin);
+
+            }
+
+
+            self.valueChangedListener = function (event) {
+                var newCountry = event.detail.value;
+                var oldCountry = event.detail.previousValue;
+                
+                console.log("country changed to:"+newCountry);                
+                var message = {
+                    "message": {
+                        "value": newCountry
+                    },
+                    "mydata": {
+                        "param1": 42, "param2": "train"
+                    }
+                };
+                self.callParent(message);
+  
+            }
+/*
+            self.country.subscribe(function (newValue) {
+                console.log("The country's new name is " + newValue);
+          });
+*/
+            self.init = function () {
                 // attach listener to receive message from parent; this is not required for sending messages to the parent window
                 window.addEventListener("message", function (event) {
                     console.log("Iframe receives message from parent" + event.data);
-                    if (event.data && event.data.eventType =='countryChanged' && event.data.payload) {
+                    if (event.data && event.data.eventType == 'countryChanged' && event.data.payload) {
                         self.country(event.data.payload);
                     }
                 },
-                false);
+                    false);
             } //init
-            $(document).ready(function(){ self.init(); })
+            $(document).ready(function () { self.init(); })
 
-            }
+        }
 
         return new WorkareaViewModel();
     }
